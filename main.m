d0a1a55daeee82008bc70ef5d785e35935763d22
prashantsaroj <prashantsaroj2006@gmail.cojm>
2018-12-28 17:56:41 +0530

    Added script to run OCR

---
+++ main.m
@@ -0,0 +1,34 @@
+clear ; close all; clc
+
+input_layer_size  = 400;  % 20x20 Input Images of Digits
+hidden_layer_size = 25;   % 25 hidden units
+num_labels = 10;          % 10 labels, from 1 to 10, 0 mapped to 10
+
+load('ex4data1.mat');
+m = size(X, 1);
+
+lambda = 3;
+
+initial_Theta1 = randInitializeWeights(input_layer_size, hidden_layer_size);
+initial_Theta2 = randInitializeWeights(hidden_layer_size, num_labels);
+
+initial_nn_params = [initial_Theta1(:) ; initial_Theta2(:)];
+
+options = optimset('MaxIter', 50);
+
+% Create "short hand" for the cost function to be minimized
+costFunction = @(p) nnCostFunction(p, ...
+                                   input_layer_size, ...
+                                   hidden_layer_size, ...
+                                   num_labels, X, y, lambda);
+
+[nn_params, cost] = fmincg(costFunction, initial_nn_params, options);
+
+% Obtain Theta1 and Theta2 back from nn_params
+Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), ...
+                 hidden_layer_size, (input_layer_size + 1));
+
+Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):end), ...
+                 num_labels, (hidden_layer_size + 1));
+
+fprintf('\nTraining Set Accuracy: %f\n', mean(double(pred == y)) * 100);
